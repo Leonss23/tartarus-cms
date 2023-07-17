dcb2ebcb95df8a507f54beec693a539a17ec6fa6
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2023-07-17 00:42:19 -0300

    fixed and properly built `StoreSelector` Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,25 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import { Navbar } from "@/components/Navbar";
 import { StoreSelector } from "@/components/StoreSelector";
+import { getStores } from "@/lib/db/getStores";
 
-export function Header() {
+export async function Header() {
+  const { userId } = auth();
+  if (!userId) redirect("/sign-in");
+
+  const { data: stores } = await getStores(userId);
+  if (!stores) redirect("/");
+
   return (
-    <nav className="border-b-2 flex p-4 items-baseline gap-4">
+    <header className="border-b-2 flex p-4 items-baseline gap-4">
       <h1 className="text-4xl font-black">Tartarus</h1>
-      <StoreSelector />
+      <StoreSelector stores={stores} />
       <Navbar className="" />
       <div className="ml-auto flex items-center">
         <UserButton afterSignOutUrl="/" />
       </div>
-    </nav>
+    </header>
   );
 }
--- src/components/StoreSelector.tsx
+++ src/components/StoreSelector.tsx
@@ -1,3 +1,76 @@
-export function StoreSelector() {
-  return <p>StoreSelector</p>
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { ComponentPropsWithoutRef, useState } from "react";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "./ui/command";
+import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Store } from "@/db/drizzle/schema";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+type Props = ComponentPropsWithoutRef<typeof PopoverTrigger> & {
+  stores: Store[];
+};
+
+export function StoreSelector({ stores = [], className }: Props) {
+  const storeModal = useStoreModal();
+  const selectedId = Number(useParams().storeId);
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          // aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {stores.find((store) => store.id === selectedId)?.name}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search store..." />
+          <CommandEmpty>No stores found.</CommandEmpty>
+          <CommandGroup>
+            {stores.map((store) => (
+              <CommandItem
+                key={store.id}
+                onSelect={() => {
+                  console.log({ store });
+                  setOpen(false);
+                  router.push(`/${store.id}`);
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    selectedId === store.id ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {store.name}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
 }
