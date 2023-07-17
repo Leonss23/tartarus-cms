d7ff6d3e8057483e7d7ae3bc8ac34fa2084519ea
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2023-07-17 00:38:44 -0300

    Exporting schema types

--- src/db/drizzle/schema.ts
+++ src/db/drizzle/schema.ts
@@ -1,16 +1,33 @@
-import { mysqlTable, mysqlSchema, AnyMySqlColumn, uniqueIndex, serial, varchar, timestamp } from "drizzle-orm/mysql-core"
-import { sql } from "drizzle-orm"
+import {
+  mysqlTable,
+  mysqlSchema,
+  AnyMySqlColumn,
+  uniqueIndex,
+  serial,
+  varchar,
+  timestamp,
+} from "drizzle-orm/mysql-core";
+import { InferModel, sql } from "drizzle-orm";
 
+export const cmsStore = mysqlTable(
+  "cms_store",
+  {
+    id: serial("id").primaryKey().notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    author: varchar("author", { length: 255 }).notNull(),
+    createdAt: timestamp("created_at", { mode: "string" })
+      .defaultNow()
+      .notNull(),
+    updatedAt: timestamp("updated_at", { mode: "string" })
+      .defaultNow()
+      .onUpdateNow()
+      .notNull(),
+  },
+  (table) => {
+    return {
+      name: uniqueIndex("name").on(table.name),
+    };
+  }
+);
 
-export const cmsStore = mysqlTable("cms_store", {
-	id: serial("id").primaryKey().notNull(),
-	name: varchar("name", { length: 255 }).notNull(),
-	author: varchar("author", { length: 255 }).notNull(),
-	createdAt: timestamp("created_at", { mode: 'string' }).defaultNow().notNull(),
-	updatedAt: timestamp("updated_at", { mode: 'string' }).defaultNow().onUpdateNow().notNull(),
-},
-(table) => {
-	return {
-		name: uniqueIndex("name").on(table.name),
-	}
-});
+export type Store = InferModel<typeof cmsStore>;
