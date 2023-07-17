6c687088f0b4257581bbde8768cc4b761ec8e71d
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2023-07-17 00:40:28 -0300

    added check for storeId slug, must be a number

--- src/app/(dashboard)/[storeId]/layout.tsx
+++ src/app/(dashboard)/[storeId]/layout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 import { Header } from "@/components/Header";
+import { redirect } from "next/navigation";
 
 interface Props {
   children: React.ReactNode;
   params: { storeId: number };
 }
 
-export default async function Layout({ children, params }: Props) {
+export default async function Layout({ children, params: { storeId } }: Props) {
+  const slug = Number(storeId);
+  if (!slug) redirect(`/`);
+
   return (
     <>
       <Header />
